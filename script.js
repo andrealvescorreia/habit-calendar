d555e4ae0a72606ea88f8bcd53d0560628108eab
André Alves Correia <andre2013aguabranca@gmail.com>
2022-09-14 12:25:33 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,77 @@
+const successColor = String(getComputedStyle(document.body).getPropertyValue('--success-color'));
+const failureColor = String(getComputedStyle(document.body).getPropertyValue('--failure-color'));
+const neutralColor = String(getComputedStyle(document.body).getPropertyValue('--neutral-color'));
+
+
+
+
+let september = [/* 30 days */
+    0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0
+]/*0: grey, 1: green, -1: red */
+
+let firstDayOfMonth = 5// Thursday
+
+
+document.getElementById('day-1').style.gridColumnStart = firstDayOfMonth;/* 1(Sunday), ... , 7(Saturday) */
+
+function updateHabitPercentage(){
+    const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+    
+    let numOfSuccesfulDays = countOccurrences(september, 1)
+    let numOfFailedDays = countOccurrences(september, -1)
+
+
+    let SuccesPercentage = (numOfSuccesfulDays * 100) / (numOfSuccesfulDays+numOfFailedDays)
+    document.querySelector('#succes-percentage').innerHTML = SuccesPercentage
+}
+
+
+const dayBttns= document.querySelectorAll('.day')
+dayBttns.forEach( button =>{
+    button.addEventListener("click", ()=>{changeDayState(button)} )
+    button.style.backgroundColor = neutralColor
+})
+function changeDayState(dayButton){
+    const day = parseInt( dayButton.getInnerHTML())
+    const bgColor = dayButton.style.backgroundColor
+
+    if(day > september.length) return
+
+    if(bgColor == neutralColor){
+        dayButton.style.backgroundColor = successColor
+        september[day-1] = 1
+    }
+    else if(bgColor == successColor){
+        dayButton.style.backgroundColor = failureColor
+        september[day-1] = -1
+    }
+    else if(bgColor == failureColor){
+        dayButton.style.backgroundColor = neutralColor
+        september[day-1] = 0
+    }else{return}
+    updateHabitPercentage()
+}
+
+
+/* deactivate the day bttns that are not in the month (ex: february only has 28 days, so day 29, 30 and 31 should be deactivated)*/
+for (let i = 31; i > september.length; i--) {
+    const button = dayBttns[i - 1];
+    button.disabled = true;
+    button.style.backgroundColor = 'white';
+    button.innerHTML = ''
+
+}
+
+
+
+/*let obj = {
+    name:'Maykzao',
+    age:'29'
+}
+
+var jsonData = JSON.stringify(obj);
+
+localStorage.setItem('obj', jsonData);
+
+console.log(JSON.parse(localStorage.getItem('obj')))
+console.log(obj)*/
