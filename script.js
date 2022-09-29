8fcce35b87b0f98beaad006e68c5e5a50ab7f04d
André Alves Correia <andre2013aguabranca@gmail.com>
2022-09-29 14:50:54 -0300

    started implementation of Month class

--- script.js
+++ script.js
@@ -2,6 +2,91 @@ const successColor = String(getComputedStyle(document.body).getPropertyValue('--
 const failureColor = String(getComputedStyle(document.body).getPropertyValue('--failure-color'));
 const neutralColor = String(getComputedStyle(document.body).getPropertyValue('--neutral-color'));
 
+class Month {
+
+    #id
+    #daysArray
+
+    // to-do: validate if the year-month the user gave is valid.
+    constructor(id) {// use this constructor when creating a totally new month object   
+        this.#id = id;
+        this.#daysArray = this.#createDaysArray();
+    }
+
+    constructor(id, daysArray) {// use this if you are retrieving info from previously created month, for exemple a month from a json.
+        this.#id = id;
+        this.#daysArray = daysArray;
+    }
+
+    #createDaysArray(){
+        return Array(parseInt(this.#countDaysInMonth())).fill(0)
+    }
+    #countDaysInMonth() { 
+        const date = new Date(this.#id + '-1')
+        return new Date(date.getFullYear(), date.getMonth()+1, 0).getDate(); 
+    } 
+    
+   
+
+
+    getNumOfDays(){
+        return this.#daysArray.length
+    }
+    getId(){
+        return this.#id
+    }
+    getDaysArray(){
+        return this.#daysArray
+    }
+
+
+    getName(){
+        const date = new Date(this.#id + '-1')
+        return date.toLocaleString('default', { month: 'long' });
+    }
+
+    getYear(){
+        const date = new Date(this.#id + '-1')
+        return date.getFullYear()
+    }
+    getFirstDay(){// 0 (Sunday), ... , 6 (Saturday)
+        const date = new Date(this.#id + '-1')
+        return date.getDay()
+    }
+
+    stringify(){
+        let info = ''
+        info += '#id: ' + this.#id
+    }
+
+
+    getJson(){
+        let simpleMonth = {
+            id: this.#id,
+            daysArray: this.#daysArray
+        }
+        
+        let jsonData = JSON.stringify(simpleMonth);
+        return jsonData
+    }
+
+}
+
+
+
+let month = new Month('2022-09')
+console.log(month.getNumOfDays() + ' '+month.getName() +' '+ month.getYear())
+console.log(month.getDaysArray())
+console.log(month.getFirstDay())
+console.log(month.getJson())
+
+
+const monthPicker = document.getElementById('month-picker');
+monthPicker.addEventListener('change', (event) => {changed()});
+
+function changed(){
+    console.log(monthPicker.value )
+}
 
 
 
@@ -30,19 +115,19 @@ dayBttns.forEach( button =>{
 
 function changeDayState(dayButton){
     const day = parseInt( dayButton.getInnerHTML())
-    const bgColor = dayButton.style.backgroundColor
+    const currentBgColor = dayButton.style.backgroundColor
 
     if(day > september.length) return
 
-    if(bgColor == neutralColor){
+    if(currentBgColor == neutralColor){
         dayButton.style.backgroundColor = successColor
         september[day-1] = 1
     }
-    else if(bgColor == successColor){
+    else if(currentBgColor == successColor){
         dayButton.style.backgroundColor = failureColor
         september[day-1] = -1
     }
-    else if(bgColor == failureColor){
+    else if(currentBgColor == failureColor){
         dayButton.style.backgroundColor = neutralColor
         september[day-1] = 0
     }else{return}
@@ -68,8 +153,10 @@ function updateHabitPercentage(){
     let SuccesPercentage = (numOfSuccesfulDays * 100) / (numOfSuccesfulDays+numOfFailedDays)
     document.querySelector('#succes-percentage').innerHTML = String(SuccesPercentage)+'%'
 }
+/*
 
-/*let obj = {
+
+let obj = {
     name:'Maykzao',
     age:'29'
 }
