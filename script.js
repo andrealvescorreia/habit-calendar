d1d604b9761da70c753e474f2fe66215b8546b95
André Alves Correia <andre2013aguabranca@gmail.com>
2022-10-12 00:37:59 -0300

    refactored code
    now is more readable  and clean.

--- script.js
+++ script.js
@@ -1,95 +1,91 @@
-var successColor = String(getComputedStyle(document.body).getPropertyValue('--success-color'));
-var failureColor = String(getComputedStyle(document.body).getPropertyValue('--failure-color'));
-var numberColor  = String(getComputedStyle(document.body).getPropertyValue('--number-color'));
-var numberColorTransparent = String(getComputedStyle(document.body).getPropertyValue('--number-color-transparent'));
+
+var currentlyDisplayingMonth// type: Month
+
+// COLORS:
+var successColor
+var failureColor
+var numberColor
+var numberColorTransparent
+
+function updateColorsValues(){
+    successColor = String(getComputedStyle(document.body).getPropertyValue('--success-color'));
+    failureColor = String(getComputedStyle(document.body).getPropertyValue('--failure-color'));
+    numberColor  = String(getComputedStyle(document.body).getPropertyValue('--number-color'));
+    numberColorTransparent = String(getComputedStyle(document.body).getPropertyValue('--number-color-transparent'));
+}
+updateColorsValues()
 
 
-var displayingMonth
+// BUTTONS:
+const dayButtons                = document.querySelectorAll('.day-button');
+const bttnMonthSwitcherPrevious = document.getElementById('month-switcher-left');
+const bttnMonthSwitcherNext     = document.getElementById('month-switcher-right');
+const bttnTrashCan              = document.getElementById('trash-bttn')
+const bttnDarkModeToggle        = document.getElementById('dark-mode-toggle');
 
 
-const dayBttns = document.querySelectorAll('.day')
-dayBttns.forEach( button =>{
-    button.addEventListener("click", ()=>{changeDayState(button)} )
-})
-
-const successPercentageNumberDisplay = document.getElementById('succes-percentage-number')
-const monthSwitcherPrevious = document.getElementById('month-switcher-left');
-const monthSwitcherNext = document.getElementById('month-switcher-right');
-const trashCan = document.getElementById('trash-bttn')
-const darkModeToggle = document.getElementById('dark-mode-toggle');
 
 
-darkModeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    if (window.localStorage.getItem("theme") === "dark") {
-        window.localStorage.setItem("theme", "light");
-    } else {
-        window.localStorage.setItem("theme", "dark")
-    }
-    updateDisplayTheme()
+// _____EVENT LISTENERS:
+
+dayButtons.forEach( button =>{
+    button.addEventListener("click", ()=>{
+        switchDayState(button)
+        updateCalendarDisplay()
+    })
 });
 
-trashCan.addEventListener("click", ()=>{clearDisplayingMonth()});
+bttnDarkModeToggle.addEventListener("click", () => {
+    themeSwitcher()
+    updateCalendarDisplay()
+});
+
+bttnTrashCan.addEventListener("click", ()=>{
+    clearDataFromCurrentlyDisplayingMonth()
+    updateCalendarDisplay()
+});
+
+bttnMonthSwitcherPrevious.addEventListener("click", ()=>{
+    let previousMonthId
+    if(currentlyDisplayingMonth.getNumber() - 1 > 0){
+        previousMonthId = currentlyDisplayingMonth.getYear() + '-' + String(currentlyDisplayingMonth.getNumber() - 1).padStart(2, '0')   
+    } 
+    else {
+        previousMonthId = currentlyDisplayingMonth.getYear() - 1 + '-12'
+    }
+    changeCurrentlyDisplayingMonth(previousMonthId)
+    updateCalendarDisplay()
+})
+
+bttnMonthSwitcherNext.addEventListener("click", ()=>{
+    let nextMonthId
+    if(currentlyDisplayingMonth.getNumber() + 1 <= 12){
+        nextMonthId = currentlyDisplayingMonth.getYear() + '-' + String(currentlyDisplayingMonth.getNumber() + 1).padStart(2, '0')   
+    } 
+    else {
+        nextMonthId = currentlyDisplayingMonth.getYear() + 1 + '-01'
+    }
+    changeCurrentlyDisplayingMonth(nextMonthId)
+})
 
 
-// makes sure that, the default displaying month when the page is loaded, it's the one the user is living in at the moment.
+
+
+
+
+// makes sure that, the default displaying month when the page is loaded, it's the one the user is living on at the moment.
 const today = new Date()
 const todayMonthId = String( today.getFullYear()+ '-' + String(today.getMonth() + 1).padStart(2, '0'));
+changeCurrentlyDisplayingMonth(todayMonthId)
 
-const existingMonth = getMonth(todayMonthId)
-if(existingMonth){
-    displayingMonth = existingMonth
-} 
-else {
-    displayingMonth = new Month(todayMonthId)
-    localStorage.setItem(displayingMonth.getId(), displayingMonth.getJson());
-}
-updateSelectedMonthDisplay()
-
+// switch to dark theme if it was the theme the user was using before.
 if(window.localStorage.getItem("theme") === "dark"){
     document.body.classList.toggle("dark");
-    updateDisplayTheme()
+    updateColorsValues()
+    updateCalendarDisplay()
 }
 
-monthSwitcherPrevious.addEventListener("click", ()=>{
-    let previousId
-    if(displayingMonth.getNumber() - 1 > 0){
-        previousId = displayingMonth.getYear() + '-' + String(displayingMonth.getNumber() - 1).padStart(2, '0')   
-    } 
-    else {
-        previousId = displayingMonth.getYear() - 1 + '-12'
-    }
-    const existingMonth = getMonth(previousId)
-    if(existingMonth){
-        displayingMonth = existingMonth
-    } 
-    else {
-        newMonth = new Month(previousId)
-        displayingMonth = newMonth
-        localStorage.setItem(displayingMonth.getId(), displayingMonth.getJson());
-    }
-    updateSelectedMonthDisplay()
-})
 
-monthSwitcherNext.addEventListener("click", ()=>{
-    let nextId
-    if(displayingMonth.getNumber() + 1 <= 12){
-        nextId = displayingMonth.getYear() + '-' + String(displayingMonth.getNumber() + 1).padStart(2, '0')   
-    } 
-    else {
-        nextId = displayingMonth.getYear() + 1 + '-01'
-    }
-    const existingMonth = getMonth(nextId)
-    if(existingMonth){
-        displayingMonth = existingMonth
-    } 
-    else {
-        newMonth = new Month(nextId)
-        displayingMonth = newMonth
-        localStorage.setItem(displayingMonth.getId(), displayingMonth.getJson());
-    }
-    updateSelectedMonthDisplay()
-})
 
 
 
@@ -101,24 +97,66 @@ monthSwitcherNext.addEventListener("click", ()=>{
 // ______FUNCTIONS______
 
 
+function changeCurrentlyDisplayingMonth(monthId){
+    const existingMonth = getMonth(monthId)
+    if(existingMonth){
+        currentlyDisplayingMonth = existingMonth
+    } 
+    else {
+        newMonth = new Month(monthId)
+        currentlyDisplayingMonth = newMonth
+        localStorage.setItem(currentlyDisplayingMonth.getId(), currentlyDisplayingMonth.getJson());
+    }
+    updateCalendarDisplay()
+}
+
+
 function getMonth(id){// returns false if not found it
     let simpleMonth = JSON.parse(localStorage.getItem(id))
     if(simpleMonth == null) return false
     let foundMonth = new Month(simpleMonth.id, simpleMonth.daysArray)
-    return foundMonth
+    return foundMonth;
 }
 
 
-
-function updateSelectedMonthDisplay(){
+function updateCalendarDisplay() {
+    updateSuccessPercentageDisplay()
+    updateDayButtonsDisplay()
+    document.getElementById('day-1').style.gridColumnStart = currentlyDisplayingMonth.getFirstDay()+1;/* 1(Sunday), ... , 7(Saturday) */
+    document.getElementById('month-name').innerText = currentlyDisplayingMonth.getName() + ' ' + currentlyDisplayingMonth.getYear()
     
+    const currentTheme = window.localStorage.getItem("theme")
+    if(currentTheme === "light"){
+        document.querySelectorAll('.bttn-svg').forEach(button =>{
+            button.style.filter = "none";
+        })
+    }else if(currentTheme === "dark"){
+        document.querySelectorAll('.bttn-svg').forEach(button =>{
+            button.style.filter = "invert(100%)";
+        })
+    } 
+}
 
-    document.getElementById('day-1').style.gridColumnStart = displayingMonth.getFirstDay()+1;/* 1(Sunday), ... , 7(Saturday) */
-    document.getElementById('month-name').innerText = displayingMonth.getName() + ' ' + displayingMonth.getYear()
 
-    // reseting the values of every 'day' button to default.
+// subfunction of 'updateCalendarDisplay()'.
+function updateSuccessPercentageDisplay(){
+    // 'countOccurrences' works as a function.
+    const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
+    const successPercentageDisplay  = document.getElementById('success-percentage-number')
+    const auxDaysArray = currentlyDisplayingMonth.getDaysArray()
+    const numOfSuccesfulDays = countOccurrences(auxDaysArray, 1)
+    let succesPercentage = (numOfSuccesfulDays * 100) / auxDaysArray.length
+    successPercentageDisplay.innerHTML = String(parseInt(succesPercentage))
+}
+
+
+// subfunction of 'updateCalendarDisplay()'.
+function updateDayButtonsDisplay(){
+
+    // reseting the values of all 'day' buttons to default:
     dayCount = 0
-    dayBttns.forEach( button =>{
+    dayButtons.forEach( button =>{
         dayCount++;
         button.innerText= dayCount;
         button.disabled = false
@@ -130,11 +168,10 @@ function updateSelectedMonthDisplay(){
         button.style.color = numberColor  
     })
 
-    // updating colors of the buttons
-    const aux = displayingMonth.getDaysArray()
+    // updating colors of all the 'day' buttons:
+    const aux = currentlyDisplayingMonth.getDaysArray()
     for(let i = 0; i < aux.length; i++){
-        const button = dayBttns[i]
-        
+        const button = dayButtons[i]
         button.style.borderStyle = "solid"
         if((aux[i] == 1)){    
             button.style.borderColor = successColor
@@ -146,13 +183,13 @@ function updateSelectedMonthDisplay(){
         }
         
         //if the displaying month has already passed for the user (chronologically):
-        if((new Date(todayMonthId+'-1')).getTime() > (new Date(displayingMonth.getId()+'-1')).getTime() ){
+        if((new Date(todayMonthId+'-1')).getTime() > (new Date(currentlyDisplayingMonth.getId()+'-1')).getTime() ){
             button.style.fontWeight  = "700"
             button.style.color = numberColorTransparent
         }
 
         //if the displaying month is the one the user is living at the moment:
-        else if(displayingMonth.getId() == todayMonthId){ 
+        else if(currentlyDisplayingMonth.getId() == todayMonthId){ 
             if(i < today.getDate() - 1){
                 button.style.fontWeight  = "700"
                 button.style.color = numberColorTransparent
@@ -163,89 +200,58 @@ function updateSelectedMonthDisplay(){
         }
     }
 
-    // hide the day bttns that are not in the month (ex: february only has 28 days, so day 29, 30 and 31 should be deactivated)
-    for (let i = 31; i > displayingMonth.getNumOfDays(); i--) {
-        const button = dayBttns[i - 1];
+    // hide the 'day' bttns that are not in the month (ex: february only has 28 days, so day 29, 30 and 31 should be deactivated)
+    for (let i = 31; i > currentlyDisplayingMonth.getNumOfDays(); i--) {
+        const button = dayButtons[i - 1];
         button.innerHTML = ''
         button.style.position = "absolute"
         button.style.zIndex = "-1";
         button.disabled = true;  
     }
-    updateHabitPercentage()
 }
 
 
-function updateDisplayTheme(){
-    numberColor = String(getComputedStyle(document.body).getPropertyValue('--number-color'));
-    numberColorTransparent = String(getComputedStyle(document.body).getPropertyValue('--number-color-transparent'));
-    const currentTheme = window.localStorage.getItem("theme")
-    if(currentTheme === "light"){
-        document.querySelectorAll('.bttn-svg').forEach(button =>{
-            button.style.filter = "none"
-        })
-    }else if(currentTheme === "dark"){
-        document.querySelectorAll('.bttn-svg').forEach(button =>{
-            button.style.filter = "invert(100%)"
-        })
-    }
-    updateSelectedMonthDisplay()
+// dark mode functionality
+function themeSwitcher(){
+    document.body.classList.toggle("dark");
+    if ( window.localStorage.getItem("theme") === "dark" ) 
+        window.localStorage.setItem("theme", "light");
+    else 
+        window.localStorage.setItem("theme", "dark");
+
+    updateColorsValues()
 }
 
-function clearDisplayingMonth(){// trashCan
-    const cleanMonth = new Month(displayingMonth.getId())
-    localStorage.removeItem(displayingMonth.getId())
-    displayingMonth = cleanMonth
-    updateSelectedMonthDisplay()
+
+// trash-can functionality
+function clearDataFromCurrentlyDisplayingMonth(){
+    const cleanMonth = new Month(currentlyDisplayingMonth.getId())
+    localStorage.removeItem(currentlyDisplayingMonth.getId())
+    currentlyDisplayingMonth = cleanMonth
 }
 
-function changeDayState(dayButton){
-    const day = parseInt(dayButton.getInnerHTML())
+
+function switchDayState(dayButton){
+    const dayNumber = parseInt(dayButton.getInnerHTML())
     const currentBorderColor = dayButton.style.borderColor
     
-    if(day > displayingMonth.getNumOfDays()) return
+    if(dayNumber > currentlyDisplayingMonth.getNumOfDays()) return
     
-    if(dayButton.style.borderStyle == "none"){
+    if(dayButton.style.borderStyle == "none"){// switch to failure state.
         dayButton.style.borderStyle = "solid"
         dayButton.style.borderColor = failureColor
-        displayingMonth.changeDaysArray(day-1, -1)
-        localStorage.setItem(displayingMonth.getId(), displayingMonth.getJson());
+        currentlyDisplayingMonth.changeDaysArray(dayNumber-1, -1)
+        localStorage.setItem(currentlyDisplayingMonth.getId(), currentlyDisplayingMonth.getJson());
     }
-    else if(currentBorderColor == failureColor){     
+    else if(currentBorderColor == failureColor){// switch to success state. 
         dayButton.style.borderColor = successColor
-        displayingMonth.changeDaysArray(day-1, 1)
-        localStorage.setItem(displayingMonth.getId(), displayingMonth.getJson());  
+        currentlyDisplayingMonth.changeDaysArray(dayNumber-1, 1)
+        localStorage.setItem(currentlyDisplayingMonth.getId(), currentlyDisplayingMonth.getJson());  
     }
-    else if(currentBorderColor == successColor){
+    else if(currentBorderColor == successColor){// switch back to neutral state.
         dayButton.style.borderStyle = "none"
-        displayingMonth.changeDaysArray(day-1, 0)
-        localStorage.setItem(displayingMonth.getId(), displayingMonth.getJson());   
+        currentlyDisplayingMonth.changeDaysArray(dayNumber-1, 0)
+        localStorage.setItem(currentlyDisplayingMonth.getId(), currentlyDisplayingMonth.getJson());   
     } 
     else return
-    updateHabitPercentage()
 }
-
-
-function updateHabitPercentage(){
-    
-    // 'countOccurrences' works as a function.
-    const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
-    
-    const auxDaysArray = displayingMonth.getDaysArray()
-    const numOfSuccesfulDays = countOccurrences(auxDaysArray, 1)
-    SuccesPercentage = (numOfSuccesfulDays * 100) / auxDaysArray.length
-    successPercentageNumberDisplay.innerHTML = String(parseInt(SuccesPercentage))
-}
-
-
-/*
-function allStorage() {
-    let values = [],
-        keys = Object.keys(localStorage),
-        i = keys.length;
-
-    while ( i-- ) {
-        values.push( localStorage.getItem(keys[i]) );
-    }
-    return values;
-}
-*/
